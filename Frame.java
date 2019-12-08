5f1b965d73289381c3f9aebf9798c18bd49c4c91
SamsonRollo <58102742+SamsonRollo@users.noreply.github.com>
2019-12-08 15:27:50 +0800

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Frame extends JFrame{
 
     public Frame(){
-        setSize(887,525);
+        setSize(900,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setUndecorated(true);
