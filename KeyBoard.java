7df442a5bf48736ab7df46775f0560229f4d413a
SamsonRollo <58102742+SamsonRollo@users.noreply.github.com>
2019-12-12 14:01:17 +0800

    animation sprite update and keyboard controls

---
+++ KeyBoard.java
@@ -0,0 +1,52 @@
+import java.awt.event.*;
+
+public class KeyBoard implements KeyListener, FocusListener {
+	
+	public boolean[] keys = new boolean[120];
+	private Game game;
+
+	public KeyBoard(Game game) {
+		this.game = game;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if(key<keys.length)
+			keys[key] = true;
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();	
+		if(key<keys.length)
+			keys[key] = false;
+	}
+
+	public void keyTyped(KeyEvent e) {}
+
+	public void focusLost(FocusEvent f) {
+		for(int i = 0; i<keys.length; i++)
+			keys[i] = false;
+	}
+
+	public void focusGained(FocusEvent f) {}
+
+	public boolean up() {
+		return keys[KeyEvent.VK_UP];
+	}
+
+	public boolean down() {
+		return keys[KeyEvent.VK_DOWN];
+	}
+
+	public boolean left() {
+		return keys[KeyEvent.VK_LEFT];
+	}
+
+	public boolean right() {
+		return keys[KeyEvent.VK_RIGHT];
+	}
+
+	public boolean enter() {
+		return keys[KeyEvent.VK_ENTER];
+	}
+}
