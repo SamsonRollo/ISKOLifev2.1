28ba660af6b550c5dafd070a6f4585bc2827b036
SamsonRollo <58102742+SamsonRollo@users.noreply.github.com>
2019-12-15 01:17:36 +0800

    Update Mob.java

--- Mob.java
+++ Mob.java
@@ -3,37 +3,43 @@ public class Mob extends Character {
 	private int cycle = 0;
 	private int tempdir = 4;
 	protected int speed = 1;
+	public static boolean running = false;
 
 	public Mob(Sprite sprite, int posx, int posy, int xZoom, int yZoom, int sizex, int sizey, int colx, int coly, Game game){
 		setCharacter(sprite, posx, posy, xZoom, yZoom, sizex, sizey, colx, coly, game);
 	}
 
-
 	@Override
-	public void update(Game game) {
-		int newDir = cycle;
+	public void update(Game game) { //copy to player in implementing the ai
+		int newDir = 0;
 		int pastX = playerBody.x;
 		int pastY = playerBody.y;	
 
-		if(cycle==0){//d
-			xOffset=5;
-			yOffset=-7;
-			playerBody.y +=speed;
-		}
-		else if(cycle==1){//l
-			yOffset=-7; 
-			xOffset=5;
-			playerBody.x -= speed;
-		}
-		else if(cycle==2){//r
-			yOffset=-7; 
-			xOffset=5;
-			playerBody.x += speed;
-		}
-		else{//u
-			xOffset=5;
-			yOffset=-7;
-			playerBody.y -=speed;
+		if(running){
+			if(cycle==0){//d check
+				newDir = 0;
+				xOffset=5;
+				yOffset=-7;
+				playerBody.y +=speed;
+			}
+			else if(cycle==1){//l check
+				newDir = 1;
+				yOffset=-7; 
+				xOffset=5;
+				playerBody.x -= speed;
+			}
+			else if(cycle==2){//r
+				newDir = 2;
+				yOffset=-7; 
+				xOffset=5;
+				playerBody.x += speed;
+			}
+			else{//u
+				newDir = 3;
+				xOffset=5;
+				yOffset=-7;
+				playerBody.y -=speed;
+			}
 		}
 
 		if(newDir!=direction){
@@ -41,34 +47,39 @@ public class Mob extends Character {
 			updateDirection();
 		}
 
-		collisionCheckRectangle.x = playerBody.x + xOffset;
-		collisionCheckRectangle.y = playerBody.y + 30 + yOffset;
+		if(!running)
+			animatedSprite.reset();
 
-		Rectangle newCollisionRect = new Rectangle(collisionCheckRectangle.x, collisionCheckRectangle.y, collisionCheckRectangle.w, collisionCheckRectangle.h);
 
-		boolean xMove = game.getMap().checkCollision(newCollisionRect);
-		boolean yMove = game.getMap().checkCollision(newCollisionRect);
+		if(running){
+			collisionCheckRectangle.x = playerBody.x + xOffset;
+			collisionCheckRectangle.y = playerBody.y + 30 + yOffset;
 
-		if(xMove || yMove){
-			if(xMove) 
-				playerBody.x = pastX;
-			
-			if(yMove)
-				playerBody.y = pastY;
+			Rectangle newCollisionRect = new Rectangle(collisionCheckRectangle.x, collisionCheckRectangle.y, collisionCheckRectangle.w, collisionCheckRectangle.h);
+
+			boolean xMove = game.getMap().checkCollision(newCollisionRect);
+			boolean yMove = game.getMap().checkCollision(newCollisionRect);
+
+			if(xMove || yMove){
+				if(xMove) 
+					playerBody.x = pastX;
 				
-			 cycle = (int) Math.floor((tempdir)%4);
-			 tempdir++;
+				if(yMove)
+					playerBody.y = pastY;
+					
+				 cycle = (int) Math.floor((tempdir)%4);
+				 tempdir++;
+			}
+
+			animatedSprite.update(game);
 		}
-
-		System.out.println(cycle+" "+tempdir);
-
-		animatedSprite.update(game);
+		running =false;
 	}
 
 	@Override
 	protected void updateDirection() {
 		if(animatedSprite!=null) {
-			animatedSprite.setAnimateRange(direction*6, (direction*6)+6);
+			animatedSprite.setAnimateRange(direction*8, (direction*8)+8);
 		}
 	}
 }
